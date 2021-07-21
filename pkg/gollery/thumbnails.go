2e963f723735d834d1b12364e2195c92edd648c9
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2021-07-20 22:12:35 -0300

    Make one single video thumbnail

--- pkg/gollery/thumbnails.go
+++ pkg/gollery/thumbnails.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 
 	"gopkg.in/vansante/go-ffprobe.v2"
 )
@@ -73,35 +74,64 @@ func (f File) GenThumbnails(dir string, force bool) error {
 
 		totalLength := data.Format.DurationSeconds
 
-		const thumbCount = 10
-		const videoDuration float64 = 5
+		const segmentCount = 12
+		const segmentDuration float64 = 4
 
 		margins := .1 // 10%
 		start := margins * float64(totalLength)
 		length := float64(totalLength) - start // Cut the start
 
+		var segments []string
+
 		// Make small video segments
-		for i := 0; i < thumbCount; i++ {
+		for i := 0; i < segmentCount; i++ {
+			fname := path.Join(name, fmt.Sprintf("thumb-segment-%03d.ts", i+1))
+			segments = append(segments, fname)
 			makeThumb := exec.Command(
 				"ffmpeg",
 				"-ss",
-				fmt.Sprintf("%f", start+length*(float64(i)/thumbCount)),
+				fmt.Sprintf("%f", start+length*(float64(i)/segmentCount)),
 				"-i",
 				f.Path,
 				"-map",
 				"0:v:0",
 				"-t",
-				fmt.Sprintf("%f", videoDuration),
+				fmt.Sprintf("%f", segmentDuration),
 				"-vf",
 				"scale=-2:240",
 				"-preset",
 				"veryfast",
-				path.Join(name, fmt.Sprintf("thumb-%03d.mp4", i+1)),
+				"-c:v",
+				"libx264",
+				"-crf",
+				"19",
+				fname,
 			)
 
 			err := makeThumb.Run()
 			if err != nil {
 				os.RemoveAll(name)
+				return err
+			}
+		}
+
+		// Join the segments
+		err = exec.Command(
+			"ffmpeg",
+			"-i",
+			fmt.Sprintf("concat:%s", strings.Join(segments, "|")),
+			"-c",
+			"copy",
+			path.Join(name, "thumb.mp4"),
+		).Run()
+		if err != nil {
+			return err
+		}
+
+		// Delete the individual segments
+		for _, fname := range segments {
+			err := os.Remove(fname)
+			if err != nil {
 				return err
 			}
 		}
